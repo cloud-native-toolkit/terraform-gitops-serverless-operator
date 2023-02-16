e85493b98d205b0fdc338da93b4350f95de97f16
Andrew Trice <amtrice@us.ibm.com>
2023-02-16 09:24:14 -0500

    more debug info
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -14,6 +14,15 @@ echo "DEST_DIR: $DEST_DIR"
 echo "MODULE_DIR: $MODULE_DIR"
 echo "CHART_DIR: $CHART_DIR"
 
+echo "ls $CHART_DIR"
+ls $CHART_DIR
+
+echo "ls $DEST_DIR"
+ls $DEST_DIR
+
+echo "pwd:"
+pwd
+
 ## Add logic here to put the yaml resource content in DEST_DIR
 
 cp -R "${CHART_DIR}"/* "${DEST_DIR}"
