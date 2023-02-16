68f32498613e86c867dcaf6b12da2b978cd2306c
Andrew Trice <amtrice@us.ibm.com>
2023-02-16 09:08:18 -0500

    added debug info to create-yaml script
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -7,6 +7,13 @@ CHART_DIR=$(cd "${MODULE_DIR}/charts/knative-serving-operator"; pwd -P)
 NAME="$1"
 DEST_DIR="$2"
 
+## Debug
+
+echo "NAME: $NAME"
+echo "DEST_DIR: $DEST_DIR"
+echo "MODULE_DIR: $MODULE_DIR"
+echo "CHART_DIR: $CHART_DIR"
+
 ## Add logic here to put the yaml resource content in DEST_DIR
 
 cp -R "${CHART_DIR}"/* "${DEST_DIR}"
