8175193506149b2709b53c12e76cdde814dd5b81
Andrew Trice <amtrice@us.ibm.com>
2023-02-17 11:04:03 -0500

    remove prefix truncation
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- test/stages/stage1-gitops-bootstrap.tf
+++ test/stages/stage1-gitops-bootstrap.tf
@@ -13,7 +13,7 @@ module "gitops-bootstrap" {
   bootstrap_path      = module.gitops.bootstrap_path
   sealed_secret_cert  = module.cert.cert
   sealed_secret_private_key = module.cert.private_key
-  prefix              = substr(var.bootstrap_prefix, 0, 62)
+  prefix              = var.bootstrap_prefix
   kubeseal_namespace  = var.kubeseal_namespace
   create_webhook      = true
 }
